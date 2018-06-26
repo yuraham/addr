164f04f3219036659477fcc730fff7f2785d98d1
yuraham <yuraham@naver.com>
2018-06-26 10:17:22 +0900

    주소록 데이터 등록

--- addr/models.py
+++ addr/models.py
@@ -3,13 +3,13 @@ from django.utils import timezone
 
 
 class Address(models.Model):
+    writer = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     num = models.IntegerField()
     num2 = models.IntegerField(blank=True, null=True)
     email = models.EmailField(blank=True, null=True)
     save_date = models.DateTimeField(default=timezone.now())
-    writer = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    secret = models.BooleanField(default=False)
+    published_date = models.DateTimeField(blank=True, null=True)
 
     def __str__(self):
         return self.name
--- addr/views.py
+++ addr/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Address
+from django.utils import timezone
 
-# Create your views here.
+
+def addr_list(request):
+    addrs = Address.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/addr_list.html', {'addrs':addrs})
+
